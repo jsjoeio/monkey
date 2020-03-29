86730e7857dad56203201ae5cd5a3ed17de7c800
Joe Previte <jjprevite@gmail.com>
2020-03-28 20:03:54 -0700

    add mod file and update lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -15,13 +15,13 @@ func New(input string) *Lexer {
 	return l
 }
 
-func (l *lexer) NextToken() token.Token {
+func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
-	case ':':
+	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
