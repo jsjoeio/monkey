425b7d39cd8c1a6cb377c21825a2a1b209515d6f
Joe Previte <jjprevite@gmail.com>
2020-03-29 20:01:25 -0700

    fix: lexer test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -80,12 +80,23 @@ if (5 < 10) {
 		{token.RT, ">"},
 		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
-		// add tokens for this 
-		// if (5 < 10) {
-		// 	return true;
-		// } else {
-		// 	return false;
-		// }
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
 		{token.INT, "10"},
 		{token.EQ, "=="},
 		{token.INT, "10"},
