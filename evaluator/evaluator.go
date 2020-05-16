4207f2dd1b5e9aac68e0272c942748cb75756ec1
Joe Previte <jjprevite@gmail.com>
2020-05-16 11:35:28 -0700

    feat: add hash literals to evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -86,6 +86,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 				return evalIdentifier(node, env)
 	case *ast.StringLiteral:
 			return &object.String{Value: node.Value}
+	case *ast.HashLiteral:
+			return evalHashLiteral(node, env)
 	case *ast.ReturnStatement:
 			val := Eval(node.ReturnValue, env)
 			if isError(val) {
@@ -378,4 +380,33 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
 		return NULL
 	}
 	return arrayObject.Elements[idx]
+}
+
+func evalHashLiteral(
+	node *ast.HashLiteral,
+	env *object.Environment,
+) object.Object {
+	pairs := make(map[object.HashKey]object.HashPair)
+
+	for keyNode, valueNode := range node.Pairs {
+		key := Eval(keyNode, env)
+		if isError(key) {
+			return key
+		}
+
+		hashKey, ok := key.(object.Hashable)
+		if !ok {
+			return newError("unusable as hash key: %s", key.Type())
+		}
+
+		value := Eval(valueNode, env)
+		if isError(value) {
+			return value
+		}
+
+		hashed := hashKey.HashKey()
+		pairs[hashed] = object.HashPair{Key: key, Value: value}
+	}
+
+	return &object.Hash{Pairs: pairs}
 }
