51142a45f579c14e3184c94ee15367d4297bace3
Joe Previte <jjprevite@gmail.com>
2020-05-20 20:40:27 -0700

    feat: add parser support for macros

--- ast/ast.go
+++ ast/ast.go
@@ -334,3 +334,25 @@ func (hl *HashLiteral) String() string {
 
 	return out.String()
 }
+
+type MacroLiteral struct {
+	Token token.Token
+	Parameters []*Identifier
+	Body *BlockStatement
+}
+
+func (ml *MacroLiteral) expressionNode() {}
+func (ml *MacroLiteral) TokenLiteral() string { return ml.Token.Literal }
+func (ml *MacroLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	out.WriteString(ml.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(ml.Body.String())
+
+	return out.String()
+}
